719a5c546a7f6a063af99e0392a0195b8a649bb8
matheos123 <matheosbelay@gmail.com>
2024-01-26 11:38:08 +0300

    search manufucturer page

--- components/index.ts
+++ components/index.ts
@@ -2,9 +2,15 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import NavBar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufucturer";
 export {
     Hero,
     CustomButton,
     NavBar,
-    Footer
+    Footer,
+    CustomFilter,
+    SearchBar,
+    SearchManufacturer
 }
